529b41277195c536938de87fa4090c60b0c4c792
kyao1 <164566650+kyao1@users.noreply.github.com>
2026-09-01 16:55:47 -0400

    Add temperature conversion from Celsius to Fahrenheit

---
+++ script.js
@@ -0,0 +1,8 @@
+let celsiusTemperature = 25;
+console.log(celsiusTemperature);
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+console.log(celsiusToFahrenheit(celsiusTemperature));
+console.log(celsiusToFahrenheit(0));
+console.log(celsiusToFahrenheit(-40));
